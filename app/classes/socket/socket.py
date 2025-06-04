5ab66010c20d029029c4a7cbd3970f66d65c9e26
simy46 <missipsa.annane@polymtl.ca>
2025-06-04 18:06:01 -0400

    abstract socket class

---
+++ app/classes/socket/socket.py
@@ -0,0 +1,9 @@
+class SocketService:
+    def __init__(self, socketio): 
+        self.socketio = socketio
+
+    def listen(self, event_name, callback):
+        self.socketio.on(event_name)(callback)
+
+    def send(self, event, data, room=None):
+        self.socketio.emit(event, data, room=room)
