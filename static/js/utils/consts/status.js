a293bc3c8da3ae829580019544964663f0493325
simy46 <missipsa.annane@polymtl.ca>
2025-06-10 02:13:42 -0400

    consts

---
+++ static/js/utils/consts/status.js
@@ -0,0 +1,16 @@
+export const MSG_STATUS = {
+    CLOSED: 'closed', // success
+    REQUESTED: 'requested',
+    NEW: 'new',
+    CANCELLED: 'cancelled',
+    CANCEL: 'cancel',
+    ERROR: 'error',
+    LOADED: 'loaded',
+    LOAD: 'load',
+    WILCO: 'wilco',
+    STANDBY: 'standby',
+    UNABLE: 'unable',
+    EXECUTED: 'executed',
+    EXECUTE: 'execute',
+    TIMEOUT: 'timeout'
+};
