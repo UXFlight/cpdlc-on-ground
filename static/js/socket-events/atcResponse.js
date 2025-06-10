b182dc7c5225c5ad1dd8b8f527c8d729caa42c2d
simy46 <missipsa.annane@polymtl.ca>
2025-06-10 02:14:51 -0400

    ws listens instead of http res

--- static/js/socket-events/atcResponse.js
+++ static/js/socket-events/atcResponse.js
@@ -4,12 +4,15 @@ import { filterHistoryLogs } from "../events/filter.js";
 import { playNotificationSound } from "../ui/ui.js";
 
 export const handleAtcResponse = (data) => {
-    console.log("ATC Response:", data);
-    const cancelBtn = document.querySelector(`.cancel-button[data-requesttype="${data.request}"]`);
+    const cancelBtn = document.querySelector(`.cancel-button[data-requesttype="${data.requestType}"]`);
     if (cancelBtn) cancelBtn.disabled = true;
-    updateStep(data.requestType, data.status, data.message,data.timestamp, 90);
+    updateStep(data.requestType, data.status, data.message,data.timestamp, data.timeLeft);
     playNotificationSound();
     if(state.isFiltered) return appendToLog(data.requestType, data.message, data.timestamp, data.status);
     state.isFiltered = true;
     filterHistoryLogs();
+    if (data.requestType === "pushback") {
+        const direction = data.direction;
+        // document.getElementById(`pushback-${direction}`).disabled = true;
+    }
 }
--- static/js/socket-events/requestResponse.js
+++ static/js/socket-events/requestResponse.js
@@ -1,25 +1,13 @@
 import { state, updateStep } from "../state/state.js";
-import { MSG_STATUS } from "../state/status.js";
+import { MSG_STATUS } from '../utils/consts/status.js';
 import { showTick } from "../ui/ui.js";
 import { filterHistoryLogs } from "../events/filter.js";
 
 export const handleRequestAck = (data) => {
     const { requestType, status, message, timestamp } = data;
-
     const cancelBtn = document.querySelector(`.cancel-button[data-requesttype="${requestType}"]`);
-
-    console.log(data)
-    // if (response.error || !response.status) {
-    //   showTick(requestType, true);
-    //   closeCurrentOverlay();
-    //   if (cancelBtn) cancelBtn.disabled = true;
-    //   updateStep(requestType, MSG_STATUS.ERROR, response.error || "Request failed");
-    //   return;
-    // }
-
     updateStep(requestType, status, message, timestamp);
     if (cancelBtn) cancelBtn.disabled = false;
     if (state.steps[requestType].status === MSG_STATUS.CANCELLED) return;
-    showTick(requestType);
     filterHistoryLogs();
 }
