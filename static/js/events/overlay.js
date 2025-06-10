d7ee949a2f3a8b071e78501965304df204f1ce6a
simy46 <missipsa.annane@polymtl.ca>
2025-06-10 02:15:18 -0400

    connection status

--- static/js/events/overlay.js
+++ static/js/events/overlay.js
@@ -1,11 +1,9 @@
 import { state } from '../state/state.js';
-import { MSG_STATUS } from '../state/status.js';
+import { MSG_STATUS } from '../utils/consts/status.js';
 import { checkNewRequest, closeCurrentOverlay } from '../utils/utils.js';
 
 // overlay event
-export const toggleOverlay = (overlay, e) => {
-  e.stopPropagation();
-
+export const toggleOverlay = (overlay) => {
   document.querySelectorAll(".overlay.open").forEach(open => {
     open.classList.remove("open");
   });
@@ -17,7 +15,28 @@ export const toggleOverlay = (overlay, e) => {
 };
 
 
-export const closeOverlay = (event) => {
-  if (event.target.closest(".overlay")) return;
-  closeCurrentOverlay();
+export const handleGlobalClick = (event) => {
+  const isInsideOverlay = event.target.closest(".overlay");
+  const isInsideConnection = event.target.closest("#connection-status");
+  if (!isInsideConnection) document.getElementById("connection-status")?.classList.remove("show-tooltip");
+  if (!isInsideOverlay) closeCurrentOverlay();
+};
+
+// FOR MOBILE DEVICES
+export const touchStartEvent = (e) => {
+    const el = e.target.closest(".overlay");
+    if (!el) return;
+    el.classList.add("touched");
+    setTimeout(() => { el.classList.remove("touched") }, 150);
 }
+
+export const touchFeedbackButtons = (e) => {
+  const btn = e.target.closest('.request-button, .cancel-button');
+  if (!btn || btn.disabled) return;
+
+  btn.classList.add('touched');
+
+  setTimeout(() => {
+    btn.classList.remove('touched');
+  }, 120);
+};
