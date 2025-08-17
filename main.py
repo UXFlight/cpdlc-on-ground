5b9be05934de0d60e2fd8e2d5e7a0bde9ba64422
simy46 <missipsa.annane@polymtl.ca>
2025-08-17 17:32:50 -0400

    logger

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import mimetypes
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
-from app.classes.ingsvc.agent import Echo
-from app.classes.socket.socket import SocketService
+from app.classes.socket import SocketService
 from app.managers import PilotManager, SocketManager, PilotStats
 from app.routes import general
-from app.classes.gss.gss_client import gss_client
+from app.classes.gss_client import gss_client
 
 exit_event = threading.Event()
 
@@ -17,11 +16,8 @@ def create_app():
     mimetypes.add_type('application/javascript', '.js')
     app = Flask(__name__, static_url_path="/static", static_folder="static")
     CORS(app)
-    socketio = SocketIO(app, cors_allowed_origins="*")
+    socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
     return app, socketio
-
-def start_ingscape():
-    Echo.start_ingescape_agent()
 
 def signal_handler(sig, frame):
     print("\n[System] Ctrl+C detected, exiting...")
@@ -33,22 +29,28 @@ if __name__ == '__main__':
 
     app, socketio = create_app()
 
-    # ingescape_thread = threading.Thread(target=start_ingscape)
-    # ingescape_thread.start()
-
-    gss_client.connect()
+    import logging
+    logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
     socket_service = SocketService(socketio)
     pilot_manager = PilotManager()
     pilot_stats = PilotStats(pilot_manager)
 
     general.pilot_stats = pilot_stats
+    general.pilot_manager = pilot_manager
+    general.socket_service = socket_service
+    general.gss_client = gss_client
     app.register_blueprint(general.general_bp)
 
     socket_manager = SocketManager(socket_service, pilot_manager)
     socket_manager.init_events()
 
+    gss_client.connect()
+
     try:
+        host = "0.0.0.0"
+        port = 5321
+        print(f"[SERVER] Server running at http://localhost:{port}")
         socketio.run(app, host="0.0.0.0", port=5321, use_reloader=False, allow_unsafe_werkzeug=True)
     except KeyboardInterrupt:
         pass
