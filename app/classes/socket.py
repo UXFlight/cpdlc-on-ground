90d9a92966a07f4ce8983d5845347de511abdf33
simy46 <missipsa.annane@polymtl.ca>
2025-08-18 13:04:22 -0400

    added rooms for atcs

--- app/classes/socket.py
+++ app/classes/socket.py
@@ -1,9 +1,21 @@
+from flask_socketio import SocketIO, join_room, leave_room
+
 class SocketService:
     def __init__(self, socketio): 
-        self.socketio = socketio
+        self.socketio : SocketIO = socketio
 
     def listen(self, event_name, callback):
         self.socketio.on(event_name)(callback)
 
     def send(self, event, data, room=None): 
-        self.socketio.emit(event, data, room=room)
+        self.socketio.emit(event, data, to=room)
+
+    def enter_room(self, sid, room):
+        join_room(room, sid=sid)
+
+    def leave_room(self, sid, room):
+        leave_room(room, sid=sid)
+
+    # security helper
+    def disconnect(self, sid: str):
+        self.socketio.disconnect(sid)
