89688527178e633d7941aa3f0fb901d3624f2379
simy46 <missipsa.annane@polymtl.ca>
2025-08-18 17:12:40 -0400

    corrected init state so no None attributs

--- app/classes/step.py
+++ app/classes/step.py
@@ -1,24 +1,24 @@
 from dataclasses import dataclass, field
 from typing import Optional, List
 import threading
-from app.utils.types import StepEvent, StepStatus, UpdateStepData
+from app.utils.types import StepEvent, StepPublicView, StepStatus, UpdateStepData
 
 
 @dataclass
 class Step:
     step_code: str
     label: str
-    status: StepStatus = StepStatus.IDLE
-    message: Optional[str] = ""
-    timestamp: Optional[float] = None
-    validated_at: Optional[float] = None
     request_id: str = ""
+    status: StepStatus = StepStatus.IDLE
+    message: str = ""
+    timestamp: float = 0.0
+    validated_at: float = 0.0
     time_left: Optional[float] = None
 
     # === Internal state
     history: List[StepEvent] = field(default_factory=list)
     lock: threading.Lock = field(default_factory=threading.Lock, repr=False)
-
+    
     # === Apply update and add to history
     def apply_update(self, update: UpdateStepData) -> None:
         with self.lock:
@@ -31,7 +31,8 @@ class Step:
 
             # Save event in step history
             self.history.append(StepEvent(
-                status=update.status,
+                step_code=update.step_code,
+                status=update.status.value,
                 timestamp=update.validated_at,
                 message=update.message
             ))
@@ -40,7 +41,8 @@ class Step:
     def add_history(self, update: UpdateStepData) -> None:
         with self.lock:
             self.history.append(StepEvent(
-                status=update.status,
+                step_code=update.step_code,
+                status=update.status.value,
                 timestamp=update.validated_at,
                 message=update.message
             ))
@@ -50,8 +52,8 @@ class Step:
         with self.lock:
             self.status = StepStatus.IDLE
             self.message = ""
-            self.timestamp = None
-            self.validated_at = None
+            self.timestamp = 0.0
+            self.validated_at = 0.0
             self.request_id = ""
             self.time_left = None
             self.history.clear()
@@ -68,20 +70,16 @@ class Step:
             "request_id": self.request_id,
             "time_left": self.time_left
         }
-
-    # === Init from GSS StepPublicView
-    @staticmethod
-    def from_public_view(data: dict) -> "Step":
-        return Step(
-            step_code=data["step_code"],
-            label=data["label"],
-            status=StepStatus(data["status"]),
-            message=data.get("message", ""),
-            timestamp=data.get("timestamp"),
-            validated_at=data.get("validated_at"),
-            request_id=data.get("request_id", ""),
-            time_left=data.get("time_left")
-        )
+        
+    def to_step_public_view(self) -> StepPublicView:
+        return {
+            "step_code": self.step_code,
+            "label": self.label,
+            "status": self.status.value,
+            "message": self.message,
+            "validated_at": self.validated_at,
+            "time_left": self.time_left
+        }
 
     # === Init from UpdateStepData
     @staticmethod
