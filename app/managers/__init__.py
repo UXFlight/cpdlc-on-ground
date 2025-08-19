c866e4567decb93c52e914f35d2dd73a1ca07e10
simy46 <missipsa.annane@polymtl.ca>
2025-08-19 00:32:36 -0400

    airport map supprt + 3 airports

--- app/managers/__init__.py
+++ app/managers/__init__.py
@@ -3,3 +3,4 @@ from app.managers.pilot_manager import PilotManager
 from app.managers.socket_manager import SocketManager
 from app.managers.timer_manager import TimerManager
 from app.managers.atc_manager import AtcManager
+from app.managers.airport_map_manager import AirportMapManager
